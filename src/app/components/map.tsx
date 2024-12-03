7ccfd7dca51e2e403378232932b3a063e5c6761b
Yen Dah Hsiang <dustin0116@gmail.com>
2024-12-03 02:46:13 -0800

    Store API Key in .env

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -4,7 +4,7 @@ import { Map, ZoomControl, Overlay } from 'pigeon-maps'
 import { maptiler } from 'pigeon-maps/providers'
 import { fetchData } from '../services/api'
 
-const maptilerProvider = maptiler('captR7Gr1UNOIhWTwXle', 'streets');
+const maptilerProvider = maptiler(process.env.NEXT_PUBLIC_MAPTILER_API_KEY as string, 'streets');
 const vancouverCenter: [number, number] = [49.24669, -123.12092]; // Latitude and longitude of South Cambie (Center of Vancouver)
 
 const neighborhoodCoordinates: { [key: string]: [number, number] } = {
